dea05faf50650e5638f54e031462e7cfb5a3d472
Syed Tahmeer Hussain Naqvi <tahmeerhussain1@gmail.com>
2025-10-20 22:32:08 +0500

    master ki on load pr revert kr dia

--- resources/views/websitePages/master.blade.php
+++ resources/views/websitePages/master.blade.php
@@ -1959,13 +1959,7 @@
         }, 1000);
       });
     });
-    window.addEventListener("load", function() {
-        const preloader = document.getElementById("preloader");
-        preloader.style.opacity = "0";
-        setTimeout(() => {
-        preloader.style.display = "none";
-        }, 500); 
-    });
+    
 
   });
 
